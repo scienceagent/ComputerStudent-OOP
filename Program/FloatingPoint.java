2097cbcc608431fb2c29bec8c70165f70c4a218b
science.kot <pseudo.denver@gmail.com>
2021-01-08 20:18:01 +0200

    another program

---
+++ Program/FloatingPoint.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+    double myDouble = 3.4;
+    float myFloat = 3.4F;
+
+
+    double myDoubleScientific = 3.445e2;
+
+    System.out.println(myDouble);
+    System.out.println(myFloat);
+    System.out.println(myDoubleScientific);   
+  }
+}
