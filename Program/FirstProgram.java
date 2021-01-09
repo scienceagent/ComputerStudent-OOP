07d7e213c8f13b786c25e1aedad1556563be2b95
scienceagent <pseudo.denver@gmail.com>
2021-01-09 22:00:45 +0200

    change

--- Program/FirstProgram.java
+++ Program/FirstProgram.java
@@ -1,7 +1,7 @@
 /**
  * First Program
  */
-public class Hello {
+public class FirstProgram {
 
     public static void main(String[] args) {
         System.out.println("First Java Program");
---
+++ Program/FloatingPoint.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+
+    double myDouble = 8.5;
+    float myFloat = 8.5F;
+
+
+    double myDoubleScientific = 8.889e2;
+
+    System.out.println(myDouble);
+    System.out.println(myFloat);
+    System.out.println(myDoubleScientific);
+  }
+}
